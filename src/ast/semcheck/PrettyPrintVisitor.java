8f61994c6b5c55814afc2219bba374ae8d0b2d71
lbuttignol <leandrobuttignol@gmail.com>
2016-09-19 16:38:38 -0300

    Fix bugs

--- src/ast/semcheck/PrettyPrintVisitor.java
+++ src/ast/semcheck/PrettyPrintVisitor.java
@@ -13,7 +13,8 @@ public class PrettyPrintVisitor implements ASTVisitor {
 
 	public PrettyPrintVisitor(){
 	}
-		@Override	
+	
+	@Override	
 	public void visit(ArithmeticBinOp stmt){}
 	
 	@Override
@@ -26,7 +27,11 @@ public class PrettyPrintVisitor implements ASTVisitor {
 	public void visit(ArrayLocation stmt){}
 	
 	@Override
-	public void visit(AssignStmt stmt){}
+	public void visit(AssignStmt stmt){
+		stmt.getLocation().accept(this);
+		System.out.println(stmt.getOperator());
+		stmt.getExpression().accept(this);
+	}
 	
 	@Override
 	public void visit(AttributeArrayLocation stmt){}
@@ -35,7 +40,20 @@ public class PrettyPrintVisitor implements ASTVisitor {
 	public void visit(AttributeLocation stmt){}
 	
 	@Override
-	public void visit(Block stmt){} 		//VER
+	public void visit(Block stmt){
+		System.out.println(" { ");
+		System.out.println(" variables: ");
+		Iterator<FieldDecl> varIt = stmt.getVariable().listIterator();
+		while(varIt.hasNext()){
+			varIt.next().accept(this);
+		}
+		System.out.println(" statements: ");
+		Iterator<Statement> stmtIt = stmt.getStatements().listIterator();
+		while(stmtIt.hasNext()){
+			stmtIt.next().accept(this);
+		}
+		System.out.println(" } ");
+	}
 	
 	@Override
 	public void visit(BodyDecl stmt){}
@@ -44,19 +62,31 @@ public class PrettyPrintVisitor implements ASTVisitor {
 	public void visit(BoolenLiteral lit){}
 	
 	@Override
-	public void visit(BreakStmt stmt){}
+	public void visit(BreakStmt stmt){
+		System.out.println(stmt.toString());
+	}
 	
 	@Override
 	public void visit(ClassDecl stmt){}
 	
 	@Override
-	public void visit(ContinueStmt stmt){}
+	public void visit(ContinueStmt stmt){
+		System.out.println(stmt.toString());
+	}
 	
 	@Override
 	public void visit(EqBinOp stmt){}
 	
 	@Override
-	public void visit(FieldDecl stmt){}
+	public void visit(FieldDecl loc){
+		System.out.println(" Type ");
+		System.out.println(loc.getType());
+		System.out.println(" Declared identifiers: ");
+		Iterator<IdDecl> ideIt = loc.getName().listIterator();
+		while(ideIt.hasNext()){
+			ideIt.next().accept(this);
+		}
+	}
 	
 	@Override
 	public void visit(FloatLiteral lit){}
@@ -65,16 +95,34 @@ public class PrettyPrintVisitor implements ASTVisitor {
 	public void visit(ForStmt stmt){}
 	
 	@Override
-	public void visit(IdDecl loc){}
+	public void visit(IdDecl loc){
+		System.out.println(loc.getName()+" ,");
+	}
 	
 	@Override
-	public void visit(IfThenElseStmt stmt){}
+	public void visit(IfThenElseStmt stmt){
+		System.out.println("( if ");
+		stmt.getCondition().accept(this);
+		System.out.println(" then ");
+		stmt.getIfBlock().accept(this);
+		System.out.println(" ) else (");
+		stmt.getElseBlock().accept(this);
+		System.out.println(" ) ");
+	}
 	
 	@Override
-	public void visit(IfThenStmt stmt){}
+	public void  visit(IfThenStmt stmt){
+		System.out.println("( if ");
+		stmt.getCondition().accept(this);
+		System.out.println(" then ");
+		stmt.getIfBlock().accept(this);
+		System.out.println(" ) ");
+	}
 	
 	@Override
-	public void visit(IntLiteral lit){}
+	public void visit(IntLiteral lit){
+		System.out.println(lit.toString());
+	}
 	
 	@Override
 	public void visit(LogicalBinOp stmt){}
@@ -101,10 +149,15 @@ public class PrettyPrintVisitor implements ASTVisitor {
 	public void visit(RelationalBinOp stmt){}
 	
 	@Override
-	public void visit(ReturnStmt stmt){}
+	public void visit(ReturnStmt stmt){
+		System.out.println("return");
+		stmt.getExpression().accept(this);
+	}
 	
 	@Override
-	public void visit(ReturnVoidStmt stmt){}
+	public void visit(ReturnVoidStmt stmt){
+		System.out.println(stmt.toString());
+	}
 	
 	@Override
 	public void visit(Skip stmt){}
@@ -113,77 +166,14 @@ public class PrettyPrintVisitor implements ASTVisitor {
 	public void visit(Statement stmt){}
 	
 	@Override
-	public void visit(VarLocation loc){}
-	
-	@Override
-	public void visit(WhileStmt stmt){}
-	
-	
-	// visit statements
-	@Override
-	public void visit(AssignStmt stmt){
-		stmt.getLocation().accept(this);
-		System.out.println(stmt.getOperator());
-		stmt.getExpression().accept(this);
-	}
-
-	@Override
-	public void visit(ReturnStmt stmt){
-		System.out.println("return");
-		stmt.getExpression().accept(this);
-	}
-
-	@Override
-	public void visit(ReturnVoidStmt stmt){
-		System.out.println(stmt.toString());
-	}
-
-	@Override
-	public void  visit(IfThenStmt stmt){
-		System.out.println("( if ");
-		stmt.getCondition().accept(this);
-		System.out.println(" then ");
-		stmt.getIfBlock().accept(this);
-		System.out.println(" ) ");
-	}
-
-	@Override
-	public void visit(IfThenElseStmt stmt){
-		System.out.println("( if ");
-		stmt.getCondition().accept(this);
-		System.out.println(" then ");
-		stmt.getIfBlock().accept(this);
-		System.out.println(" ) else (");
-		stmt.getElseBlock().accept(this);
-		System.out.println(" ) ");
-	}
-
-	@Override
-	public void visit(Block stmt){
-		System.out.println(" { ");
-		System.out.println(" variables: ");
-		Iterator<FieldDecl> varIt = stmt.getVariable().listIterator();
-		while(varIt.hasNext()){
-			varIt.next().accept(this);
+	public void visit(VarLocation loc){
+		System.out.println(loc.getBlockId());
+		Iterator<String> locIt = loc.getIds().listIterator();
+		while(locIt.hasNext()){
+			System.out.println(locIt.next());
 		}
-		System.out.println(" statements: ");
-		Iterator<Statement> stmtIt = stmt.getStatements().listIterator();
-		while(stmtIt.hasNext()){
-			stmtIt.next().accept(this);
-		}
-		System.out.println(" } ");
 	}
-
-	@Override
-	public void visit(BreakStmt stmt){
-		System.out.println(stmt.toString());
-	}
-
-	@Override
-	public void visit(ContinueStmt stmt){
-		System.out.println(stmt.toString());
-	}
-
+	
 	@Override
 	public void visit(WhileStmt stmt){
 		System.out.println("( while ");
@@ -193,48 +183,4 @@ public class PrettyPrintVisitor implements ASTVisitor {
 		System.out.println(" ) ");
 	}
 	
-	@Override
-	public void visit(BinOpExpr expr){
-		System.out.println(expr.toString());
-		/*
-		expr.getLeftOperand().accept(this);
-		expr.getOperator().accept(this);
-		expr.getRightOperand().accept(this); 
-		*/
-	}
-	
-	@Override
-	public void visit(IntLiteral lit){
-		System.out.println(lit.toString());
-	}
-
-	@Override
-	public void visit(VarLocation loc){
-		System.out.println(loc.getBlockId());
-		Iterator<String> locIt = loc.getIds().listIterator();
-		while(locIt.hasNext()){
-			System.out.println(locIt.next());
-		}
-	}
-
-	@Override
-	public void visit(FieldDecl loc){
-		System.out.println(" Type ");
-		System.out.println(loc.getType());
-		System.out.println(" Declared identifiers: ");
-		Iterator<IdDecl> ideIt = loc.getName().listIterator();
-		while(ideIt.hasNext()){
-			ideIt.next().accept(this);
-		}
-	}
-
-	@Override
-	public void visit(IdDecl loc){
-		System.out.println(loc.getName()+" ,");
-	}
-
-	@Override
-	public void visit(AST ast){
-	}
-
 }
