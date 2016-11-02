995f379dc33e5a541d6220046ae97689564ddd3e
lbuttignol <leandrobuttignol@gmail.com>
2016-11-02 02:48:15 -0300

    Add compile and run script

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+comp="dist/Compiler.jar" # Jar name 
+printlib="lib/printLib.c" # lib  name
+srcfile="$1"	
+execut="pepito" # runeable file
+
+java -jar $comp $srcfile
+gcc $printlib "${srcfile%.ctds}.s" -o $execut
+./$execut
+exit 0
