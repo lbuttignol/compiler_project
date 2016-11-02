4d1befffd4b7c9b40bfd8355a2fbb3213d383c81
lbuttignol <leandrobuttignol@gmail.com>
2016-11-02 01:18:25 -0300

    fix print functions

--- lib/printLib.c
+++ lib/printLib.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 
-int extern print_int(int* val){
-	printf("%d\n", *val);
+int print_int(int val){
+	printf("%d\n", val);
 	return 0;
 }
 
-int extern print_float(float* val){
-	printf("%f\n", *val);
+int print_float(float val){
+	printf("%f\n", val);
 	return 0;
 }
 
 
-void extern print_bool(int* val){
-	if(*val == 0){
+int print_bool(int val){
+	if(val == 0){
 		printf("%s\n","true" );
 	}else{
 		printf("%s\n", "false");
